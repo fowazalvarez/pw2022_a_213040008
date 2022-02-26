83063ceaa7bf84346853a7470e9aa5ff81f41daf
fowazalvarez <fowazalvarez@gmail.com>
2022-02-26 12:50:01 +0700

    Tugas latihan tugas pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+// Pertemuan 3, membahas mengenai Struktur Kendali
+
+// Pengulangan
+
+for($i = 1; $i <= 10; $i++) {
+    echo "Hello World!";
+    echo "br";
+}
+
+// Pengkodisian
+$x = 10;
+if( $x > 5) {
+    echo "Bilangan $x lebih besar dari 5";
+}
+
+?>
