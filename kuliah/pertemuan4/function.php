0ccc1b740568fb98f551bd549af414b99e7b8486
fowazalvarez <fowazalvarez@gmail.com>
2022-03-08 23:13:53 +0700

    Menyelesaikan Tugas 4

--- kuliah/pertemuan4/function.php
+++ kuliah/pertemuan4/function.php
@@ -1,26 +1,9 @@
-<!-- <?php
-// Youtube
-// Membuat fungsi / definisi fungsi 
-function totalLuasDuaKubus($a, $b) {
-    $luas_a = $a * $a * $a;
-    $luas_b = $b * $b * $b;
-    $total= $luas_a + $luas_b;
-    return "Total luas dari kubus A dengan sisi $a dan kubus B dengan sisi $b adalah $total";
-}   
-echo totalLuasDuaKubus(9,4);
-echo "<br>";
-echo totalLuasDuaKubus(10,15);
-echo "<br>";
-echo totalLuasDuaKubus(100,200);
-?> -->
-
-<!-- LANJUTAN PENGERJAAN VID BELAJAR PHP UNTUK PEMULA -->
 <?php
+// Youtube
 function salam($waktu = "Datang", $nama = "Admin") {
     return "Selamat $waktu, $nama!";
 }
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
