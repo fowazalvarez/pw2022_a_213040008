4fc4f2b6d1bf17bdb816afc886e5f3a6b5120706
fowazalvarez <fowazalvarez@gmail.com>
2022-03-08 23:03:52 +0700

    Mengerjakan Tugas 4

---
+++ kuliah/pertemuan4/function.php
@@ -0,0 +1,70 @@
+<!-- <?php
+// Youtube
+// Membuat fungsi / definisi fungsi 
+function totalLuasDuaKubus($a, $b) {
+    $luas_a = $a * $a * $a;
+    $luas_b = $b * $b * $b;
+    $total= $luas_a + $luas_b;
+    return "Total luas dari kubus A dengan sisi $a dan kubus B dengan sisi $b adalah $total";
+}   
+echo totalLuasDuaKubus(9,4);
+echo "<br>";
+echo totalLuasDuaKubus(10,15);
+echo "<br>";
+echo totalLuasDuaKubus(100,200);
+?> -->
+
+<!-- LANJUTAN PENGERJAAN VID BELAJAR PHP UNTUK PEMULA -->
+<?php
+function salam($waktu = "Datang", $nama = "Admin") {
+    return "Selamat $waktu, $nama!";
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1><?= salam("Pagi", "Wildan"); ?></h1>
+</body>
+</html>
+
+<?php
+// LURING TANGGAL 05-Maret-2022
+// Menghitung total 2 kubus
+
+// $a = 9;
+// $b = 4;
+
+// $luas_a = $a * $a * $a;
+// $luas_b = $b * $b * $b;
+
+// $total = $luas_a + $luas_b;
+
+// echo "Total luas dari kubus A dengan sisi $a dan kubus B dengan sisi $b adalah $total";
+// echo "<hr>";
+
+// // Membuat fungsi / definisi fungsi
+// function totalLuasDuaKubus ($a, $b) {
+
+//     $luas_a = $a * $a * $a; 
+//     $luas_b = $b * $b * $b;
+
+//     $total = $luas_a + $luas_b;
+
+//     return "Total luas dari kubus A dengan sisi $a dan kubus B dengan sisi $b adalah $total";
+// }
+
+// echo totalLuasDuaKubus(9, 4);
+// echo "<br>";
+// echo totalLuasDuaKubus(10, 15);
+// echo "<br>";
+// echo totalLuasDuaKubus(100, 200);
+// echo "<hr>"; 
+?> 
