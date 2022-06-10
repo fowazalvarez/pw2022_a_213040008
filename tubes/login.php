66794898c696ee77435cc6547decc77cfb8674b3
fowazalvarez <fowazalvarez@gmail.com>
2022-06-11 02:01:50 +0700

    Menambah Log in

---
+++ tubes/login.php
@@ -0,0 +1,66 @@
+<?php 
+require 'functions.php';
+
+if (isset($_POST["login"]) ) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE username = 'username'");
+
+    // Cek Username
+    if(mysqli_num_rows($result) === 1) {
+
+        // Cek Password
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row["password"])) {
+            header("Location: index.php");
+            exit;
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+</head>
+
+<body>
+
+    <h1>Halaman Login</h1>
+
+    <form action="" method="post">
+
+        <ul>
+            <li>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+
+    </form>
+
+</body>
+
+</html>
